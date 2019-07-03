d366cebe473a12df4fcd5a88a65ce4d6b62d43e4
Tommy Ettinger <tommy.ettinger@gmail.com>
2019-07-02 21:18:53 -0700

    Update TransmissionDemo with modern Gradle and test of new JSNI

---
+++ demo/TransmissionDemo/lwjgl3/src/main/java/blazing/chain/demo/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package blazing.chain.demo.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import blazing.chain.demo.TransmissionDemo;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new TransmissionDemo(null), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("TransmissionDemo");
+        configuration.setWindowedMode(1000, 700);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
